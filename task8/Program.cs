3523acf9d82fa1083fec9016f21ea43f6f48aac3
Elena Korenko <elenakor987@gmail.com>
2022-12-03 23:30:24 +0300

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+ a на выходе показывает все четные числа от 1 до N.*/
+
+Console.WriteLine("Введите число");
+string numberStr=Console.ReadLine();
+int number=Convert.ToInt32(numberStr);
+
+for(int i=1; i<=number; i++)
+
+     if (i % 2 == 0)
+     Console.Write($"{i},");
